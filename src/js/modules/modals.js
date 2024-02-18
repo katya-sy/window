538c72c42626e450dcd61e4cdc2497911a53ae6e
katya-sy <katya_sy_28@mail.ru>
2024-02-18 19:12:26 +0900

    add modals

---
+++ src/js/modules/modals.js
@@ -0,0 +1,45 @@
+const modals = () => {
+  function openModal(modal) {
+    modal.style.display = "block";
+    document.body.style.overflow = "hidden";
+  }
+
+  function closeModal(modal) {
+    modal.style.display = "none";
+    document.body.style.overflow = "";
+  }
+
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    const trigger = document.querySelectorAll(triggerSelector),
+      modal = document.querySelector(modalSelector),
+      close = modal.querySelector(closeSelector);
+
+    trigger.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        if (e.target) e.preventDefault();
+
+        openModal(modal);
+      });
+    });
+
+    close.addEventListener("click", () => closeModal(modal));
+
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) closeModal(modal);
+    });
+  }
+
+  function showModalByTime(selector, time) {
+    setTimeout(() => {
+      const modal = document.querySelector(selector);
+      openModal(modal);
+    }, time);
+  }
+
+  bindModal(".popup_engineer_btn", ".popup_engineer", ".popup_close");
+  bindModal(".phone_link", ".popup", ".popup_close");
+
+  showModalByTime(".popup", 60000);
+};
+
+export default modals;
