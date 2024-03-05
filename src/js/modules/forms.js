94ef0cd8a252141f0610dd620b8d4fd3e8b110a4
katya-sy <katya_sy_28@mail.ru>
2024-03-05 23:18:05 +0900

    add forms

---
+++ src/js/modules/forms.js
@@ -0,0 +1,54 @@
+const forms = () => {
+  const form = document.querySelectorAll("form"),
+    inputs = document.querySelectorAll("input"),
+    phoneInputs = document.querySelectorAll("input[name='user_phone']");
+
+  phoneInputs.forEach((item) => {
+    item.addEventListener("input", () => {});
+  });
+
+  const message = {
+    loading: "Загрузка...",
+    success: "Спасибо! Скоро мы с Вами свяжемся",
+    failure: "Что-то пошло не так...",
+  };
+
+  const postData = async (url, data) => {
+    document.querySelector(".status").textContent = message.loading;
+    let result = await fetch(url, {
+      method: "POST",
+      body: data,
+    });
+
+    return await result.text();
+  };
+
+  const clearInputs = () => {
+    inputs.forEach((item) => (item.value = ""));
+  };
+
+  form.forEach((item) => {
+    item.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      item.appendChild(statusMessage);
+
+      const formData = new FormData(item);
+
+      postData("assets/server.php", formData)
+        .then((result) => {
+          console.log(result);
+          statusMessage.textContent = message.success;
+        })
+        .catch(() => (statusMessage.textContent = message.failure))
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => statusMessage.remove(), 10000);
+        });
+    });
+  });
+};
+
+export default forms;
